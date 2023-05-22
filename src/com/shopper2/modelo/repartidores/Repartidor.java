596c77824be982fa09fb3236312842fddcdb0da0
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-05-22 17:58:46 +0200

    Creo setters de las variables

--- src/com/shopper2/modelo/repartidores/Repartidor.java
+++ src/com/shopper2/modelo/repartidores/Repartidor.java
@@ -24,6 +24,22 @@ public class Repartidor implements IRepartidor {
     }
 
     /**
+     * Asignar un identificador al repartidor
+     * @param codr codigo del repartidor
+     */
+    public void setCodr(int codr) {
+        this.codr = codr;
+    }
+
+    /**
+     * Asignar un nombre al repartidor
+     * @param nomr nombre del repartidor
+     */
+    public void setNomr(String nomr) {
+        this.nomr = nomr;
+    }
+
+    /**
      * Obtener el identificador del repartidor.
      * @return identificador del repartidor.
      */
@@ -41,3 +57,5 @@ public class Repartidor implements IRepartidor {
         return nomr;
     }
 }
+
+
