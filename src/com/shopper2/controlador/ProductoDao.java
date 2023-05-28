1481c177a284d0a50efcfb17eb7ca36db3b244fc
acasella03 <114586433+acasella03@users.noreply.github.com>
2023-05-28 17:26:17 +0200

    Botón agregar y guardar los productos en la tabla tienen

--- src/com/shopper2/controlador/ProductoDao.java
+++ src/com/shopper2/controlador/ProductoDao.java
@@ -1,14 +1,38 @@
 package com.shopper2.controlador;
 
 import com.shopper2.modelo.enums.Categoria;
+import com.shopper2.modelo.pedido.Pedido;
 import com.shopper2.modelo.productos.Producto;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ProductoDao {
-
+    /**
+     * Variable auxiliar para Singleton
+     */
+    private static ProductoDao instance = null;
+    
+    /**
+     * Método estático que retorna una única instancia
+     *
+     * @return instancia única
+     */
+    public static ProductoDao getInstance(){
+        if(instance==null){
+            instance = new ProductoDao();
+        }
+        return instance;
+    }
+    /**
+     * Constructor privado para ser utilizado únicamente por el Singleton.
+     */
+    private ProductoDao(){
+        
+    }
+    
     /**
      * Ruta de ubicación de la base de datos
      */
@@ -56,15 +80,15 @@ public class ProductoDao {
      */
     public Producto buscar(int codpr) {
         connect();
-        Producto producto=new Producto();
+        Producto producto = new Producto();
         try {
             PreparedStatement sentencia = conexion.prepareStatement("SELECT * from productos where codpr=?");
             sentencia.setInt(1, codpr);
             ResultSet resultado = sentencia.executeQuery();
             if (resultado.next()) {
                 producto.setCodpr(resultado.getInt("codpr"));
-                producto.setNombreProducto(resultado.getString("nombreProducto"));
-                producto.setCategoria(Categoria.valueOf(resultado.getString("categoria")));
+                producto.setNombreProducto(resultado.getString("nompr"));
+                producto.setCategoria(Categoria.fromString(resultado.getString("categoria")));
             }
         } catch (SQLException e) {
             System.err.println(e);
@@ -73,5 +97,32 @@ public class ProductoDao {
             close();
         }
         return producto;
+    }
+
+    /**
+     * Realiza consultas de una tabla
+     *
+     * @return la lista con todos los productos
+     */
+    public ArrayList<Producto> listaProductos() {
+        connect();
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        try {
+            PreparedStatement sentencia = conexion.prepareStatement("select * from productos");
+            ResultSet resultado = sentencia.executeQuery();
+            while (resultado.next()) {
+                Producto producto = new Producto();
+                producto.setCodpr(resultado.getInt("codpr"));
+                producto.setCategoria((Categoria) (Object) resultado.getString("categoria"));
+                Pedido pedido = new Pedido();
+
+                listaProductos.add(producto);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            close();
+        }
+        return listaProductos;
     }
 }
--- src/com/shopper2/modelo/enums/Categoria.java
+++ src/com/shopper2/modelo/enums/Categoria.java
@@ -4,5 +4,26 @@ package com.shopper2.modelo.enums;
  * Clasificación de las categorías de los productos.
  */
 public enum Categoria {
-    ALIMENTACION, DROGUERIA, HIGIENE, MASCOTAS;
+
+    ALIMENTACION("alimentacion"), DROGUERIA("drogueria"), HIGIENE("higiene"), MASCOTAS("mascotas");
+
+    private final String stringValue;
+
+    private Categoria(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public static Categoria fromString(String stringValue) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.getStringValue().equals(stringValue)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("No Such Value");
+
+    }
 }
